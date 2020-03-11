12949649cc986713b34a243bf5b491e46720189c
Tom Claes <tom.claes@cegeka.com>
2020-03-12 00:46:34 +0100

    remove prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,15 +7,12 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    'comma-dangle': ['error', 'ignore']
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
 };
