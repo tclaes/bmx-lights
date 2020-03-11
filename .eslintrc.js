ad9031861fe9cf8561d45045245353a80552db2f
Tom Claes <tom.claes@cegeka.com>
2020-03-12 00:49:09 +0100

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
