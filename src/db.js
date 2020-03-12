8704c266859f512d8f2a3d5bfd002fe9ebd41d53
Tom Claes <tom.claes@cegeka.com>
2020-03-12 21:55:23 +0100

    env variables && added now.sh

--- src/db.js
+++ src/db.js
@@ -4,9 +4,9 @@ import "firebase/auth";
 
 // Get a Firestore instance
 const fb = firebase.initializeApp({
-  apiKey: "",
-  authDomain: "bmx-tracks.firebaseapp.com",
-  projectId: "bmx-tracks"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID
 });
 
 export const db = fb.firestore();
